8828898ec3ada54faf63526e546187be74a14ee2
卢升达笔记 <49872322+Thanking@users.noreply.github.com>
2020-10-14 21:53:06 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+package testjava;
+
+/**
+ * @auther mynawang
+ * @create 2016-12-14 16:21
+ */
+public class test {
+    public static void main (String[] arg) {
+        System.out.println("hello world");
+    }
+}
